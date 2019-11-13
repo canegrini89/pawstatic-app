f1c77a688560fb70f67ad0440506c8785b8c32ad
Emiliano Negrini <emi.negrini@gmail.com>
2019-11-13 16:50:57 -0300

    profile refactor

--- screens/profile.js
+++ screens/profile.js
@@ -6,106 +6,144 @@ import {
   Image,
   TouchableOpacity,
   Platform,
+  ImageBackground,
 } from 'react-native';
-import RNFetchBlob from 'rn-fetch-blob';
 import firebase from 'react-native-firebase';
 import ImagePicker from 'react-native-image-picker';
 
 const Profile = () => {
   const [user, setUser] = useState({});
   const [picture, setPicture] = useState('');
+  const [background, setBackground] = useState('');
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged(currentUser => {
-      console.log(currentUser);
       if (currentUser) {
         setUser(currentUser._user);
       }
-      // if (picture) {
-      //   currentUser.user.updateProfile({
-      //     photoURL: picture,
-      //   });
-      // }
+      firebase
+        .database()
+        .ref('users/' + currentUser._user.uid)
+        .child('profile')
+        .on('value', snap => {
+          if (snap.val().picture) {
+            setPicture(snap.val().picture);
+          } else {
+            setPicture(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRlW3r9_qcfU-6M9Qf9witE-skyTNjkbHqCfgWscQC6H96Vv-IX',
+            );
+          }
+          if (snap.val().backPicture) {
+            setBackground(snap.val().backPicture);
+          } else {
+            setBackground(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQSyo2oLyCh1yIvBqi-_tMhd6GgyAv_eAoCoZ-AvlJcK76s1u-',
+            );
+          }
+        });
     });
   }, [picture]);
 
-  const uploadImage = (uri, imageName, mime = 'image/jpg') => {
-    const Blob = RNFetchBlob.polyfill.Blob;
-    const fs = RNFetchBlob.fs;
-    window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
-    window.Blob = Blob;
+  const openPicker = value => {
+    const options = {
+      title: 'Fotoğraf Seç',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+        allowsEditing: true,
+      },
+    };
 
-    return new Promise((resolve, reject) => {
-      const uploadUri =
-        Platform.OS === 'ios' ? uri.replace('file://', '') : uri;
-      let uploadBlob = null;
-      const imageRef = firebase
-        .storage()
-        .ref('profile')
-        .child(user.uid)
-        .child(imageName);
-      fs.readFile(uploadUri, 'base64')
-        .then(data => {
-          return Blob.build(data, {type: `${mime};BASE64`});
-        })
-        .then(blob => {
-          uploadBlob = blob;
-          return imageRef.put(blob, {contentType: mime});
-        })
-        .then(() => {
-          uploadBlob.close();
-          return imageRef.getDownloadURL();
-        })
-        .then(url => {
-          resolve(url);
-          firebase
-            .database()
-            .ref('users/' + user.uid + '/profile')
-            .update({
-              picture: url,
-            });
-        })
-        .catch(error => {
-          reject(error);
-        });
+    ImagePicker.showImagePicker(options, response => {
+      console.log('response:', response);
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        var path = '';
+        // eslint-disable-next-line curly
+        if (Platform.OS === 'ios') path = response.uri.toString();
+        else {
+          path = response.path.toString();
+        }
+
+        const image = {
+          image: response.uri.toString(),
+          name: response.fileName,
+          path: path,
+        };
+        uploadImage(image, value);
+      }
     });
   };
-  const onUploadPicture = () => {
-    ImagePicker.showImagePicker(response => {
-      uploadImage(response.uri, response.fileName);
-    });
+
+  const uploadImage = (image, value) => {
+    firebase
+      .storage()
+      .ref('profile/' + user.uid + '/' + image.name)
+      .putFile(image.path)
+      .then(file => {
+        if (value !== 'back') {
+          firebase
+            .database()
+            .ref('users/' + user.uid)
+            .child('profile')
+            .update({
+              picture: file.downloadURL,
+            });
+          setPicture(file.downloadURL);
+        } else {
+          firebase
+            .database()
+            .ref('users/' + user.uid)
+            .child('profile')
+            .update({
+              backPicture: file.downloadURL,
+            });
+          setBackground(file.downloadURL);
+        }
+      });
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity
-          style={styles.headerContent}
-          onPress={onUploadPicture}>
-          <Image
-            style={styles.avatar}
-            source={{
-              uri:
-                user.photoURL ||
-                'https://bootdey.com/img/Content/avatar/avatar2.png',
-            }}
-          />
-          <Text style={styles.name}>John Doe</Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.header}
+        onPress={() => openPicker('back')}>
+        <ImageBackground
+          source={{uri: background}}
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{width: '100%', height: '100%'}}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.avatar} onPress={openPicker}>
+        <Image
+          style={styles.avatarImg}
+          source={{uri: picture}}
+          onPress={openPicker}
+        />
+      </TouchableOpacity>
+      <View style={styles.body}>
+        <View style={styles.bodyContent}>
+          <Text style={styles.name}>{user.displayName}</Text>
+          <Text style={styles.info}>{user.email}</Text>
+        </View>
       </View>
 
       <View style={styles.profileDetail}>
         <View style={styles.detailContent}>
-          <Text style={styles.title}>Following</Text>
-          <Text style={styles.count}>200</Text>
-        </View>
-        <View style={styles.detailContent}>
           <Text style={styles.title}>Followers</Text>
-          <Text style={styles.count}>200</Text>
+          <Text style={styles.count}>0</Text>
         </View>
+      </View>
+
+      <View style={styles.profileDetailRigth}>
         <View style={styles.detailContent}>
-          <Text style={styles.title}>Photos</Text>
-          <Text style={styles.count}>200</Text>
+          <Text style={styles.title}>Following</Text>
+          <Text style={styles.count}>0</Text>
         </View>
       </View>
     </View>
@@ -116,13 +154,48 @@ export default Profile;
 
 const styles = StyleSheet.create({
   header: {
-    backgroundColor: '#00CED1',
-  },
-  headerContent: {
-    padding: 30,
-    alignItems: 'center',
+    backgroundColor: '#00BFFF',
+    height: 200,
   },
   avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    marginBottom: 10,
+    alignSelf: 'center',
+    position: 'absolute',
+    marginTop: 130,
+  },
+  profileDetailRigth: {
+    alignSelf: 'flex-end',
+    marginTop: 150,
+    alignItems: 'center',
+    flexDirection: 'row',
+    position: 'absolute',
+    backgroundColor: '#ffffff',
+    borderRadius: 20,
+  },
+  profileDetail: {
+    alignSelf: 'flex-start',
+    marginTop: 150,
+    alignItems: 'center',
+    flexDirection: 'row',
+    position: 'absolute',
+    backgroundColor: '#ffffff',
+    borderRadius: 20,
+  },
+  detailContent: {
+    margin: 10,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 16,
+    color: '#00CED1',
+  },
+  count: {
+    fontSize: 18,
+  },
+  avatarImg: {
     width: 130,
     height: 130,
     borderRadius: 63,
@@ -130,59 +203,22 @@ const styles = StyleSheet.create({
     borderColor: 'white',
     marginBottom: 10,
   },
-  name: {
-    fontSize: 22,
-    color: '#FFFFFF',
-    fontWeight: '600',
-  },
-  profileDetail: {
-    alignSelf: 'center',
-    marginTop: 200,
-    alignItems: 'center',
-    flexDirection: 'row',
-    position: 'absolute',
-    backgroundColor: '#ffffff',
-    borderRadius: 30,
-    borderWidth: 0.2,
-    borderColor: 'black',
-  },
-  detailContent: {
-    margin: 10,
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 20,
-    color: '#00CED1',
-  },
-  count: {
-    fontSize: 18,
+  body: {
+    marginTop: 40,
   },
   bodyContent: {
     flex: 1,
     alignItems: 'center',
     padding: 30,
-    marginTop: 40,
   },
-  textInfo: {
-    fontSize: 18,
-    marginTop: 20,
+  name: {
+    fontSize: 26,
     color: '#696969',
+    fontWeight: '600',
   },
-  buttonContainer: {
-    marginTop: 10,
-    height: 45,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 20,
-    width: 250,
-    borderRadius: 30,
-    backgroundColor: '#00CED1',
-  },
-  description: {
-    fontSize: 20,
-    color: '#00CED1',
-    marginTop: 10,
-    textAlign: 'center',
+  info: {
+    fontSize: 18,
+    color: '#00BFFF',
+    marginTop: 8,
   },
 });
